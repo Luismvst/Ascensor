8c3ef1f85132bc2f19ebb16f3968dc74d6535c33
LuisoMartinez <luismvst@gmail.com>
2017-12-15 19:35:27 +0100

    Arreglo fallos
    Tenía un punto y coma de estos puñeteros que no se ven fácilmente

--- project_1/project_1.srcs/sources_1/new/Clock_Divider.vhd
+++ project_1/project_1.srcs/sources_1/new/Clock_Divider.vhd
@@ -33,20 +33,23 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Clock_Divider is
     Generic (
-        frec: integer:=5000000      --Valor por defecto para 1 Hz
+        frec: integer:=50000000         -- frec de 50 MHz
+        --Valor por defecto para 1 Hz
     );            
     Port ( 
         clk : IN STD_LOGIC;
         reset : IN STD_LOGIC;
-        clk_out : OUT STD_LOGIC;
+        clk_out : OUT STD_LOGIC
     );
 end Clock_Divider;
 
 architecture Behavioral of Clock_Divider is
 SIGNAL clk_sig: STD_LOGIC;
+
 begin
-    process (clk, reset)
-    variable count:integer;
+
+    process (reset, clk)
+    variable count: integer;
     
     begin
     if(reset='1')   then
