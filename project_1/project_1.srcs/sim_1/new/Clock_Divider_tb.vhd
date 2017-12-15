1a882163e0f4eca73f1fa75adbfc84a7d2cea473
LuisoMartinez <luismvst@gmail.com>
2017-12-15 19:47:01 +0100

    Creamos ClockDivider testbench
    Aprovechamos y creamos el testbench del módulo de divisor de reloj.

---
+++ project_1/project_1.srcs/sim_1/new/Clock_Divider_tb.vhd
@@ -0,0 +1,86 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 15.12.2017 19:36:14
+-- Design Name: 
+-- Module Name: Clock_Divider_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Clock_Divider_tb is
+end Clock_Divider_tb;
+
+architecture Behavioral of Clock_Divider_tb is
+
+    --Declaración de componentes
+    COMPONENT Clock_Divider 
+    PORT(
+        clk: IN STD_LOGIC;
+        reset: IN STD_LOGIC;
+        clk_out: IN STD_LOGIC;
+    );
+    end COMPONENT;
+    
+    --Inputs
+    signal clk: STD_LOGIC := '0';
+    signal reset: STD_LOGIC := '0';
+    signal clk_out: STD_LOGIC := '0';
+    
+    --Definicion de periodos de reloj
+    constant clk_period : time := 10 ns;
+    
+    begin
+        --instanciacion test
+        test : Clock_Divider PORT MAP (
+            clk  => clk,
+            reset => rest,
+            clk_out => clk_out
+        );
+        
+        --Definicion del proceso Clock
+        clk_process : process ()
+        begin
+            clk <= '0';
+            wait for clk_period/2;
+            clk <= '1';
+            wait for clk_period/2;
+        end process;
+        
+        --Stimulus process
+        stim_process : process () 
+        begin
+            --Mantener estado de reset un ratito
+            wait for 100 ns;
+            
+            --Estimulo
+            reset <= '1';
+            wait for 50 ns;
+            reset <= '0';
+            wait;
+        end process;
+        
+end Behavioral;
