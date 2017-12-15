a8a5fdd345335eb7e950b4963c1820eda1ee7429
LuisoMartinez <luismvst@gmail.com>
2017-12-15 20:17:04 +0100

    Creamos Decodificador
    Con este módulo podremos modificar los displays de 7 segmentos a nuestro
    antojo. Falta su correspondiente testbench

---
+++ project_1/project_1.srcs/sources_1/new/Decoder.vhd
@@ -0,0 +1,55 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 15.12.2017 20:00:47
+-- Design Name: 
+-- Module Name: Decoder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Decoder is
+    Port ( 
+        code : IN STD_LOGIC_VECTOR (3 downto 0);
+        led : OUT STD_LOGIC_VECTOR (6 downto 0
+    );
+end Decoder;
+
+architecture dataflow of Decoder is
+
+begin
+    WITH code SELECT 
+        led <=  "0000001"   when "0000",
+                "1001111"   when "0001",
+                "0000110"   when "0010",
+                "1001100"   when "0100",
+                "0100100"   when "0101",
+                "0100000"   when "0110",
+                "0001111"   when "0111",
+                "0000000"   when "1000",
+                "0000100"   when "1001",
+                "1111110"   when others;
+end dataflow;
