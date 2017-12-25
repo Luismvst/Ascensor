a967384844a7ce88b8e7bcefeebe77e654db11b2
LuisoMartinez <luismvst@gmail.com>
2017-12-25 21:12:10 +0100

    Cambiamos la libreria del Decoder.tb
    Las anteriores parece que no se ajustaban a los modelos elegidos

--- project_1/project_1.srcs/sim_1/new/Decoder_tb.vhd
+++ project_1/project_1.srcs/sim_1/new/Decoder_tb.vhd
@@ -1,23 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 15.12.2017 20:37:23
--- Design Name: 
--- Module Name: Decoder_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
