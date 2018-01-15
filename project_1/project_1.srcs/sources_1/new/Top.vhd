997f63a4338faf65d172b4392dfeed4019307d16
LuisoMartinez <luismvst@gmail.com>
2018-01-15 23:16:49 +0100

    FINALIZAMOS TOP
    Falta que el grupo decida cómo proseguimos la implementacion de los
    modulos. Tenemos por ahora la funcionaliad inicial. Por lo que este
    branch de TOP lo cierro.

--- project_1/project_1.srcs/sources_1/new/Top.vhd
+++ project_1/project_1.srcs/sources_1/new/Top.vhd
@@ -4,49 +4,44 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_arith.ALL;
 use ieee.std_logic_unsigned.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Top is
     Port ( 
         --Para el Decoder
-        segment : out std_logic_vector ( 6 downto 0 );
-        code    : in std_logic_vector (3 downto 0 );
+        segment : out std_logic_vector ( 6 downto 0 );  --La salida de los leds
        
        --Reloj y reset general
-        reset     : in std_logic;
+        reset     : in std_logic;  
         clk       : in std_logic; 
         
         --Para el Ascensor
-        sensor_piso : in std_logic; --Sensor que verifica su estado para realizar la operación de abrir puertas tras haber realizado el movimiento. Para no hacer fugaz la etapa.
+        
+         --Sensor que verifica su estado para realizar la operación de abrir puertas tras haber realizado el movimiento. Para no hacer fugaz la etapa.
+        sensor_piso : in std_logic; 
+        
         boton_pulsado   : in std_logic_vector (2 downto 0);     --Boton presionado en la FPGA  
-        puerta  : out std_logic;
-        abriendo_puerta : in std_logic;
-        cerrando_puerta : in std_logic;
-        piso : in std_logic_vector (3 downto 0);
-        motor_subir   : out std_logic;
-        motor_bajar : out std_logic;
-        motor_puerta_ascensor: out std_logic
+        puerta  : out std_logic;                                --Nos muestra la puerta abierta o cerrada
+        abriendo_puerta : in std_logic;                         --para no realizar etapas fugaces, el sensor que verifica que la puerta se ha abierto.
+        cerrando_puerta : in std_logic;                         --Lo mismo que abriendo puerta pero al contrario                   
+        motor_subir   : out std_logic;                          --Motor sube
+        motor_bajar : out std_logic;                            --Motor baja
+        motor_puerta_ascensor: out std_logic                    --Motor puerta ( 1 cierra,  0 abre )
         
     );
 end Top;
 
 architecture Behavioral of Top is
     
+    --Tipos de relojes
     signal clk_display: std_logic;
     signal clk_ascensor: std_logic;
     
     --Decoder
     signal modo : std_logic_vector (1 downto 0);
+    signal code : std_logic_vector (3 downto 0);
     
-    --Para el ascensor
+    --Ascensor
     signal piso_actual : std_logic_vector (2 downto 0);
+    signal piso : std_logic_vector (3 downto 0);  
     signal boton : std_logic_vector (2 downto 0);
     signal s_motor_puerta: std_logic;
     signal s_motor: std_logic_vector(1 downto 0);  
