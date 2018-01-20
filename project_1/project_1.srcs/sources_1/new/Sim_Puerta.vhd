426d77bbdc6b313f01b4d1f3762a8c87561552ae
LuisoMartinez <luismvst@gmail.com>
2018-01-20 21:25:56 +0100

    Simulacion Piso y Puerta Completados
    Completamos la implemetacion funcional de los simuladores de piso y
    ascensor

--- project_1/project_1.srcs/sources_1/new/Sim_Puerta.vhd
+++ project_1/project_1.srcs/sources_1/new/Sim_Puerta.vhd
@@ -5,14 +5,37 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Sim_Puerta is
 port (
 	sentido : in std_logic_vector (1 downto 0);
-	estado_sim : out std_logic_vector (1 downto 0);		--Estados de la puerta segun display. "00" Abierto total.
+	estado_sim : out std_logic_vector (1 downto 0);		--Estados de la puerta segun display. "11" Abierto total.
 	clk : in std_logic;
-	f.carrera : in std_logic_vector (1 downto 0)		--Sensores que indican apertura o cierre completos.
+	reset : in std_logic;
+	f_carrera : out std_logic_vector (1 downto 0)		--Sensores que indican apertura o cierre completos.
 	);
 end;
 
 architecture Behavioral of Sim_Puerta is
 
+signal estado : std_logic_vector (1 downto 0):="01";
+
 begin
+	Sim_puerta : process (clk, reset)
+	begin
+		if reset = '1' then
+			estado <= "11";
+		elsif rising_edge (clk) then
+			if sentido = "01" then
+				estado <= estado - 1;
+				if estado = "00" then
+					f_carrera <= "01";
+				end if;
+			elsif sentido <= "10" then
+				estado <= estado + 1;
+				if estado = "11" then
+					f_carrera <= "10";
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	estado_sim <= estado;
 
 end;
