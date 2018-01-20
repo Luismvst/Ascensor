50bb67e70116dfbb16c2b3e8cce1216e57399d03
LuisoMartinez <luismvst@gmail.com>
2018-01-20 19:25:32 +0100

    Terminados Motores

--- project_1/project_1.srcs/sources_1/new/Motor_Puerta.vhd
+++ project_1/project_1.srcs/sources_1/new/Motor_Puerta.vhd
@@ -4,9 +4,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Control_Motor_Puerta is
 	PORT (
-		clk, reset : in std_logic;
+		clk: in std_logic;
 		accion_motor_puerta: in std_logic_vector (1 downto 0);
-		sensor_presencia : in std_logic;
 		motor_puerta: out std_logic_vector (1 downto 0)
 		);
 end Control_Motor_Puerta;
@@ -14,15 +13,14 @@ end Control_Motor_Puerta;
 architecture Behavioral of Control_Motor_Puerta is
 
 begin
-	motor_puerta : process (clk, reset)
+	motor_puerta : process (clk)
 	begin
-		if reset = '1' then
-			motor_puerta = '0';
-		elsif rising_edge (clk) then
-			if accion_motor_puerta ='1' then
-				motor_puerta <= '1';
-			else 
-				motor_puerta <= '0';
+		if rising_edge (clk) then
+			if accion_motor_puerta ="10" then	--Abrir
+				motor_puerta <= "10";
+			elsif accion_motor_puerta = "01" then
+				motor_puerta <= "01";
+			else motor_puerta <= "00";
 			end if;
 		end if;
 	end process;
